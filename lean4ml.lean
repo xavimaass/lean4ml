18ca9e161db82c0fa21a25f3275939417b1be960
Oğuz Kaan Yüksel <okyksl@gmail.com>
2026-05-16 15:47:21 +0200

    feat: add convexity and strong convexity statements to blueprint

--- lean4ml.lean
+++ lean4ml.lean
@@ -1,8 +1,8 @@
-import lean4ml.Example
-import lean4ml.Optimization.Convex
 import lean4ml.Optimization.Defs
+import lean4ml.Optimization.Convex
 import lean4ml.Optimization.GradientFlow
 import lean4ml.Optimization.LSmooth
 import lean4ml.Optimization.LineMap
 import lean4ml.Optimization.NecessaryCondition
+import lean4ml.Optimization.StronglyConvex
 import lean4ml.Optimization.SufficientCondition
